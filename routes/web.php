a3f668593eaf640d7af8007bf6f1126fc6f02be3
Felipe Garcia <felipegogarcia@gmail.com>
2025-04-03 13:35:49 -0300

    ajuste de erro na rota

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,5 @@ use App\Http\Controllers\Api\PaymentController;
 
 // Web Routes
 Route::get('/', [CheckoutController::class, 'showCheckoutForm'])->name('checkout.form');
-Route::post('/checkout', [CheckoutController::class, 'processCheckout'])->name('checkout.process');
+Route::post('/checkout/process', [CheckoutController::class, 'processCheckout'])->name('checkout.process');
 Route::get('/thank-you', [CheckoutController::class, 'showThankYou'])->name('thank-you');
-
-
-
